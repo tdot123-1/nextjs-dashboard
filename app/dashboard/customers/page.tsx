5f209d88daed8d6a440d617db2b83e45c9434efe
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-25 13:10:47 +0200

    add customer table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,29 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
